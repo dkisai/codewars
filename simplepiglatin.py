18ec98ffe7d02a778bed1e7faccbe53b782a1103
Diego Kisai <diego.kisai@live.com>
2021-10-13 23:17:14 -0500

    Nuevo reto listo

--- simplepiglatin.py
+++ simplepiglatin.py
@@ -1,4 +1,10 @@
--
-
-
-print(pig_it("O tempora o mores !"))
+def pig_it(text):
+    pigit =''
+    splt = text.split(' ')
+    for x in splt:
+        if x == '!' or x == '?':
+            pigit += x + ' '
+        else:
+            x = x[1:len(x)]+x[0]+"ay"
+            pigit += x+' '
+    return pigit[:-1]
